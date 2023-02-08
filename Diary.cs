048a008e293eaf343ce12f424dd46efd8c0a16e4
OB1ADAET <116683120+OB1ADAET@users.noreply.github.com>
2023-02-08 13:25:17 +0300

    Add files via upload

---
+++ Diary.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp3
+{
+    public class Diary
+    {
+        public string author = string.Empty;
+        public string[] date = new string[Program.init];
+        public string[] content = new string[Program.init];
+    }
+}
